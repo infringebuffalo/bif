661a91e2630a276973ce8f149bb49d44f69050b2
davepape <depape@buffalo.edu>
2013-06-06 16:35:24 -0400

    added brochurevenues

---
+++ brochurevenues.php
@@ -0,0 +1,40 @@
+<?php
+require_once 'init.php';
+requireLogin();
+connectDB();
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-type" content="text/html;charset=utf-8" />
+<title>Buffalo Infringement Festival venues</title>
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+<h2>Venues</h2>
+
+<?php
+$stmt = dbPrepare('select name,info from venue where deleted=0 order by name');
+$stmt->execute();
+$stmt->bind_result($name,$info_ser);
+while ($stmt->fetch())
+    {
+    $info = unserialize($info_ser);
+    $address = '';
+    $website = '';
+    foreach ($info as $i)
+        {
+        if ($i[0] == 'address')
+            $address = $i[1];
+        else if ($i[0] == 'website')
+            $website = $i[1];
+        }
+    echo stripslashes($name) . '<br/>';
+    echo stripslashes($address) . '<br/>';
+    if ($website != '') echo stripslashes($website) . '<br/>';
+    echo "<br/><br/>\n";
+    }
+$stmt->close();
+?>
+
+</body>
+</html>
