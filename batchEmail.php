f4459b25c4f383e40f19acfe969feb9063932801
davepape <depape@buffalo.edu>
2014-05-15 14:08:00 -0400

    added option for comma-separated list

--- batchEmail.php
+++ batchEmail.php
@@ -9,10 +9,13 @@ if (!isset($_GET['id']))
 else
     $id = $_GET['id'];
 
+$commas = GETvalue('commas',0);
+
 $row = dbQueryByID('select name from `batch` where id=?',$id);
 bifPageheader('email for batch: ' . $row['name']);
 
 echo "<p>E-mail addresses for proposers only (not any other contacts):</p>\n";
+$output = "";
 
 $stmt = dbPrepare('select email from user join proposal on proposerid=user.id join proposalBatch on proposal.id=proposalBatch.proposal_id where proposalBatch.batch_id=? and deleted=0 order by title');
 $stmt->bind_param('i',$id);
@@ -20,9 +23,19 @@ $stmt->execute();
 $stmt->bind_result($email);
 while ($stmt->fetch())
     {
-    echo "$email<br>\n";
+    if (($commas) && (strlen($output) > 0))
+        $output .= ", ";
+    $output .= $email;
+    if (!$commas)
+        $output .= "<br>\n";
     }
 $stmt->close();
 
+echo $output;
+
+if ($commas)
+    echo "<p><a href='batchEmail.php?id=$id&commas=0'>(list without commas)</a></p>";
+else
+    echo "<p><a href='batchEmail.php?id=$id&commas=1'>(list with commas)</a></p>";
 bifPagefooter();
 ?>
