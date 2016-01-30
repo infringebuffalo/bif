4d6c82189ec157d994bffcb030694f9250cc56d8
davepape <depape@buffalo.edu>
2016-01-30 15:36:33 -0500

    add links to 2015 data

--- index.php
+++ index.php
@@ -23,11 +23,15 @@ if (hasPrivilege(array('scheduler','organizer')))
     echo <<<ENDSTRING
 <li><a href="listBatches.php">Batches</a></li>
 <li><a href="batch.php?id=0">All projects</a></li>
+<ul><li>(<a href="batch.php?id=0&festival=4200">2015 projects</a>)</li></ul>
+<ul><li>(<a href="batch.php?id=0&festival=4102">2014 projects</a>)</li></ul>
 <ul><li>(<a href="batch.php?id=0&festival=960">2013 projects</a>)</li></ul>
 <li><a href="unbatchedCheck.php">Check for unbatched projects</a></li>
 <li><a href="listDeleted.php">Deleted projects</a></li>
 <li><a href="listCategories.php">Categories (for public website)</a></li>
 <li><a href="listVenues.php">Venues</a>
+<ul><li>(<a href="listVenues.php?festival=4200">2015 venues</a>)</li></ul>
+<ul><li>(<a href="listVenues.php?festival=4102">2014 venues</a>)</li></ul>
 <ul><li>(<a href="listVenues.php?festival=960">2013 venues</a>)</li></ul>
 </li>
 <li><a href="calendar.php">Calendar</a></li>
@@ -37,57 +41,37 @@ ENDSTRING;
         echo <<<ENDSTRING
 <li><a href="listUsers.php">Users</a></li>
 <br>
-<li>Submit a proposal:
-<ul>
-<li><a href="newTheatre.php">Theatre</a></li>
-<li><a href="newMusic.php">Music</a></li>
-<li><a href="newFilm.php">Film/video</a></li>
-<li><a href="newVisualart.php">Visual art</a></li>
-<li><a href="newDance.php">Dance</a></li>
-<li><a href="newLiterary.php">Literary/poetry</a></li>
-<li><a href="newStreet.php">Street performance</a></li>
-</ul></li>
-<li><a href="newProposal.php">New proposal (for someone else)</a></li>
-<li><a href="newGroupshow.php">New group show</a></li>
-<li><a href="newBatch.php">New batch</a></li>
-<li><a href="newCategory.php">New category</a></li>
-<li><a href="newVenue.php">New venue</a></li>
-<li><a href="newBatchColumn.php">New batch column</a></li>
-<li><a href="newCard.php">New card (public contact)</a></li>
-<li><a href="subscribe.php">Add person to mailing list</a></li>
+<li><a href="Infringement_Proposal.php">Submit a proposal</a></li>
 <br>
 <li><a href="preferences.php">Preferences</a></li>
+<br>
+<li><a href="newGroupshow.php">Create a Group Show</a></li>
 <br>
 ENDSTRING;
         }
     }
 else
     {
-    echo "<li>Proposals are now closed</li><br>\n";
-/*
-    if (hasPrivilege('confirmed'))
-        {
-        echo '<li>Submit a proposal:
-<ul>
-<li><a href="newTheatre.php">Theatre</a></li>
-<li><a href="newMusic.php">Music</a></li>
-<li><a href="newFilm.php">Film/video</a></li>
-<li><a href="newVisualart.php">Visual art</a></li>
-<li><a href="newDance.php">Dance</a></li>
-<li><a href="newLiterary.php">Literary/poetry</a></li>
-<li><a href="newStreet.php">Street performance</a></li>
-</ul></li>';
-        }
-    else
-        echo '<li>Your e-mail address must be verified before you can submit a proposal: <a href="verifyEmail.php">send verification message</a></li>' . "\n";
-*/
+    
+    //echo "Proposal submissions are now open!";
+
+    //if (hasPrivilege('confirmed'))
+    //    {
+    //    echo '<br><br>
+    //      <li><a href="Infringement_Proposal.php">Submit a proposal</a></li>
+    //      <br>';
+    //    }
+    //else
+    //    echo '<li>Your e-mail address must be verified before you can submit a proposal: <a href="verifyEmail.php">send verification message</a></li><br>' . "\n";
+    //
     }
+    
 
 $festival = getFestivalID();
-$stmt = dbPrepare('select id,title from proposal where proposerid=? and festival=? and deleted=0 order by title');
+$stmt = dbPrepare('select id,title,isgroupshow from proposal where proposerid=? and festival=? and deleted=0 order by title');
 $stmt->bind_param('ii',$_SESSION['userid'],$festival);
 $stmt->execute();
-$stmt->bind_result($proposalid,$title);
+$stmt->bind_result($proposalid,$title,$isgroupshow);
 $first = true;
 while ($stmt->fetch())
     {
@@ -96,7 +80,11 @@ while ($stmt->fetch())
         echo "<li>Your proposals:<ul>\n";
         $first = false;
         }
-    echo "<li><a href='proposal.php?id=$proposalid'>$title</a></li>\n";
+		if ($isgroupshow == 0){
+          echo "<li><a href='proposal.php?id=$proposalid'>(Proposal) - $title</a></li>\n";
+	    }else{
+		  echo "<li><a href='proposal.php?id=$proposalid'>(Group Show) - $title</a></li>\n";
+		}
     }
 if (!$first)
     echo "</ul>\n";
