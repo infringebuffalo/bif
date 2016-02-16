3669be27fcaf402aff02c4e791b5f92feefb2949
davepape <depape@buffalo.edu>
2016-02-16 12:23:16 -0500

    added search-by-IP option

--- log.php
+++ log.php
@@ -8,25 +8,38 @@ bifPageheader('log');
 ?>
 <form method="POST" action="log.php">
 User (e-mail): <input type="text" name="user" size="20" value="">
-Max # of entries: <input type="text" name="limit" size="4" value="500">
+IP address: <input type="text" name="ip" size="20" value="">
 Text: <input type="text" name="text" size="20" value="">
+Max # of entries: <input type="text" name="limit" size="4" value="500">
 <input type="submit" name="submit" value="search">
 </form>
 <table>
 <?php
 $limit = POSTvalue('limit',500);
 $user = POSTvalue('user');
+$ip = POSTvalue('ip');
 $messagetext = '%' . POSTvalue('text') . '%';
-if ($user == '')
+if (($user == '') && ($ip == ''))
     {
     $stmt = dbPrepare("select time,userid,ip,proposal,is_sql,message,email,title from log left join user on userid=user.id left join proposal on log.proposal=proposal.id where message like ? order by log.id desc limit ?");
     $stmt->bind_param('si',$messagetext,$limit);
     }
-else
+else if ($ip == '')
     {
     $userid = getUserID($user);
     $stmt = dbPrepare("select time,userid,ip,proposal,is_sql,message,email,title from log left join user on userid=user.id left join proposal on log.proposal=proposal.id where message like ? and userid=? order by log.id desc limit ?");
     $stmt->bind_param('sii',$messagetext,$userid,$limit);
+    }
+else if ($user == '')
+    {
+    $stmt = dbPrepare("select time,userid,ip,proposal,is_sql,message,email,title from log left join user on userid=user.id left join proposal on log.proposal=proposal.id where message like ? and ip=? order by log.id desc limit ?");
+    $stmt->bind_param('ssi',$messagetext,$ip,$limit);
+    }
+else
+    {
+    $userid = getUserID($user);
+    $stmt = dbPrepare("select time,userid,ip,proposal,is_sql,message,email,title from log left join user on userid=user.id left join proposal on log.proposal=proposal.id where message like ? and userid=? and ip=? order by log.id desc limit ?");
+    $stmt->bind_param('sisi',$messagetext,$userid,$ip,$limit);
     }
 $stmt->execute();
 $stmt->bind_result($time,$userid,$ip,$proposal,$is_sql,$message,$username,$title);
