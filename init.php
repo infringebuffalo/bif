51fd98f19cb5e5c50f837dae6faa05bd2f5d82f3
Dave Pape <depape@buffalo.edu>
2016-03-09 08:26:41 -0500

    make sure character encodings are all utf-8

--- init.php
+++ init.php
@@ -1,6 +1,10 @@
 <?php
 ini_set('session.gc_maxlifetime', 864000);
 mb_internal_encoding('UTF-8');
+mb_http_output('UTF-8');
+mb_http_input('UTF-8'); 
+mb_language('uni');
+mb_regex_encoding('UTF-8');
 session_start();
 
 $db = false;
