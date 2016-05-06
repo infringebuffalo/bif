99dd486378bd1b9b5e6b15b8ff9c2493720fb31f
davepape <depape@buffalo.edu>
2016-05-06 19:15:35 -0400

    extra check for blank username - needed because apparently somebody created an account with a blank name ages ago, and lots of people hit the 'send me a new password' button without entering their email in the correct box

--- forgotpassword.php
+++ forgotpassword.php
@@ -5,32 +5,41 @@ connectDB();
 
 bifPageheader('forgotten password');
 
-$username = $db->real_escape_string(htmlentities(POSTvalue('username'),ENT_COMPAT | ENT_HTML5, "UTF-8"));
+$username = $db->real_escape_string(htmlentities(trim(POSTvalue('username')),ENT_COMPAT | ENT_HTML5, "UTF-8"));
 
-$data = dbQueryByString('select id from user where email=?',$username);
-if ($data)
+if ($username == '')
     {
-    $chars = array_merge(range('a', 'k'),array('m', 'n'),range('p','y'),range(3,9));
-    $password = '';
-    for ($i=0; $i < 8; $i++)
-        $password .= $chars[mt_rand(0,count($chars)-1)];
-    $encPassword = md5($password);
-    $stmt = dbPrepare('update user set newpassword=? where email=?');
-    $stmt->bind_param('ss',$encPassword,$username);
-    $stmt->execute();
-    $stmt->close();
-    log_message("generated new password for $username");
-
-    $email = $username;
-    $subject = 'Buffalo Infringement password';
-    $body = "A new password has been generated for your infringebuffalo.org login.\r\nThe new password is:\r\n" . $password . "\r\n\r\n(If you did not request a new password, you may safely ignore this message and continue to use your old password.)\r\n";
-    loggedMail($email, $subject, $body);
-    echo '<p>Done.  Check your e-mail for the new password.<br/>(Be sure to also check your spam folder if you don\'t see the message with your password soon.)</p><br/><br/><p>Contact Dave Pape [depape@buffalo.edu] with any technical problems.</p>';
+    log_message("forgotpassword failed - blank username");
+    echo "<p>You didn't enter an e-mail address - please put it in the box immediately above the \"Send me a new password\" button</p>";
     }
+
 else
     {
-    log_message("forgotpassword failed - no user '$username'");
-    echo '<p>No such account was found</p>';
+    $data = dbQueryByString('select id from user where email=?',$username);
+    if ($data)
+        {
+        $chars = array_merge(range('a', 'k'),array('m', 'n'),range('p','y'),range(3,9));
+        $password = '';
+        for ($i=0; $i < 8; $i++)
+            $password .= $chars[mt_rand(0,count($chars)-1)];
+        $encPassword = md5($password);
+        $stmt = dbPrepare('update user set newpassword=? where email=?');
+        $stmt->bind_param('ss',$encPassword,$username);
+        $stmt->execute();
+        $stmt->close();
+        log_message("generated new password for $username");
+    
+        $email = $username;
+        $subject = 'Buffalo Infringement password';
+        $body = "A new password has been generated for your infringebuffalo.org login.\r\nThe new password is:\r\n" . $password . "\r\n\r\n(If you did not request a new password, you may safely ignore this message and continue to use your old password.)\r\n";
+        loggedMail($email, $subject, $body);
+        echo '<p>Done.  Check your e-mail for the new password.<br/>(Be sure to also check your spam folder if you don\'t see the message with your password soon.)</p><br/><br/><p>Contact Dave Pape [depape@buffalo.edu] with any technical problems.</p>';
+        }
+    else
+        {
+        log_message("forgotpassword failed - no user '$username'");
+        echo "<p>No account named $username was found</p>";
+        }
     }
 
 bifPagefooter();
