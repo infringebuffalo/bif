b4a007e8c0c698606dec2b0704fb63fe367b1a81
davepape <depape@buffalo.edu>
2016-05-10 17:13:57 -0400

    added check-all and uncheck-all buttons

--- preferences.php
+++ preferences.php
@@ -4,7 +4,25 @@ connectDB();
 requireLogin();
 require_once 'util.php';
 
-bifPageheader('change preferences');
+$header = <<<ENDSTRING
+<script src="jquery.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+function unselectCheckboxes()
+    {
+    var checkers = document.getElementsByClassName('checker');
+    for (var i=0; i<checkers.length; i++)
+        checkers[i].checked = false;
+    }
+function selectCheckboxes()
+    {
+    var checkers = document.getElementsByClassName('checker');
+    for (var i=0; i<checkers.length; i++)
+        checkers[i].checked = true;
+    }
+</script>
+ENDSTRING;
+
+bifPageheader('change preferences',$header);
 
 $summaryLabels = array();
 $festival = getFestivalID();
@@ -34,11 +52,13 @@ else
 <form method="POST" action="api.php">
 <input type="hidden" name="command" value="prefsSummaryFields" />
 <input type="hidden" name="returnurl" value="." />
+<a onclick="unselectCheckboxes()" style="background:yellow">Unselect&nbsp;all</a>
+<a onclick="selectCheckboxes()" style="background:lightblue">Select&nbsp;all</a>
 <ul>
 <?php
 foreach ($summaryLabels as $s)
     {
-    echo '<li>' . $s . '<input type="checkbox" name="summaryLabel[]" value="' . htmlspecialchars($s,ENT_COMPAT | ENT_HTML5, "UTF-8") . '"';
+    echo '<li>' . $s . '<input class="checker" type="checkbox" name="summaryLabel[]" value="' . htmlspecialchars($s,ENT_COMPAT | ENT_HTML5, "UTF-8") . '"';
     if (($currentLabels==='all') || (in_array($s,$currentLabels)))
         echo " checked";
     echo " /></li>\n";
