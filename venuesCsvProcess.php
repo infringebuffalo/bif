6caa2098b696fab007ca446708142345ccd22fca
davepape <depape@buffalo.edu>
2016-05-16 00:08:21 -0400

    upload venue data from a spreadsheet

---
+++ venuesCsvProcess.php
@@ -0,0 +1,57 @@
+<?php
+require_once 'init.php';
+connectDB();
+requirePrivilege('admin');
+require_once 'util.php';
+
+log_message('uploaded spreadsheet of venue info');
+if ($_FILES)
+    {
+    bifPageheader('process venue info spreadsheet');
+    $idfieldLetter = POSTvalue('idfield','A');
+    $namefieldLetter = POSTvalue('namefield','B');
+    $shortnamefieldLetter = POSTvalue('shortnamefield','B');
+    $idfield = letterToNumber($idfieldLetter);
+    $namefield = letterToNumber($namefieldLetter);
+    $shortnamefield = letterToNumber($shortnamefieldLetter);
+    $f = $_FILES['spreadsheet'];
+    $fp = fopen($f['tmp_name'],'r');
+    $headers = fgetcsv($fp);
+    echo "<table>\n";
+    while (true)
+        {
+        $data = fgetcsv($fp);
+        if ($data === false)
+            break;
+        foreach ($data as $k=>$v)
+            $data[$k] = filter_var($v, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
+        $id = intval($data[$idfield]);
+        $name = $data[$namefield];
+        $shortname = $data[$shortnamefield];
+        $info = array();
+        foreach ($data as $key=>$val)
+            {
+            if (($key != $idfield) && ($key != $namefield) && ($key != $shortnamefield))
+                $info[] = array($headers[$key], $val);
+            }
+        $info_json = json_encode($info);
+        $stmt = dbPrepare('update venue set name=?, shortname=?, info_json=? where id=?');
+        $stmt->bind_param('sssi',$name,$shortname,$info_json,$id);
+        $stmt->execute();
+        $stmt->close();
+        echo "<tr><td>$id</td><td>$name</td><td>$shortname</td><td>$info_json</td></tr>\n";
+        }
+    echo "</table>\n";
+    fclose($fp);
+    log_message('finished processing venue spreadsheet');
+    }
+ else
+    echo '<p>nothing uploaded</p>';
+
+function letterToNumber($l)
+    {
+    return ord(strtoupper($l)) - ord('A');
+    }
+
+bifPagefooter();
+?>
---
+++ venuesCsvUpload.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'init.php';
+connectDB();
+requirePrivilege('admin');
+
+bifPageheader('upload venue info spreadsheet');
+?>
+<form method="POST" enctype="multipart/form-data" action="venuesCsvProcess.php">
+CSV file: <input type="file" name="spreadsheet" size="60">
+<br>
+ID is field: <input type="text" name="idfield" value="A">
+<br>
+Name is field: <input type="text" name="namefield" value="B">
+<br>
+Short name is field: <input type="text" name="shortnamefield" value="B">
+<p>
+<input type="submit" value="Send">
+</form>
+<?php
+bifPagefooter();
+?>
